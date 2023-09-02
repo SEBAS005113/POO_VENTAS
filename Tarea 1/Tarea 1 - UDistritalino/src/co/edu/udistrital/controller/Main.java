8592e3420ecf068036532fad83353115aa7e6c19
Alejandro-049 <143758016+Alejandro-049@users.noreply.github.com>
2023-09-02 18:08:39 -0500

    Add files via upload

--- Tarea 1/Tarea 1 - UDistritalino/src/co/edu/udistrital/controller/Main.java
+++ Tarea 1/Tarea 1 - UDistritalino/src/co/edu/udistrital/controller/Main.java
@@ -1,12 +1,5 @@
 package co.edu.udistrital.controller;
-import co.edu.udistrital.model.Cuentas;
 
 public class Main {
-
-	public static void main(String[] args) {
-		int[][] ventas = ();
-		System.out.println();
-
-	}
 
 }
--- Tarea 1/Tarea 1 - UDistritalino/src/co/edu/udistrital/model/Cuentas.java
+++ Tarea 1/Tarea 1 - UDistritalino/src/co/edu/udistrital/model/Cuentas.java
@@ -5,10 +5,10 @@ import java.util.Random;
 public class Cuentas {
 	public static int[][] ventasAleatorias() {
         int[][] ventas = new int[4][7];
-        Random random = new Random();
+        Random venta_dia = new Random();
         for (int semana = 0; semana < 4; semana++) {
             for (int dia = 0; dia < 7; dia++) {
-                ventas[semana][dia] = random.nextInt(975901) + 25000;
+                ventas[semana][dia] = venta_dia.nextInt(975901) + 25000;
             }
         }
         return ventas;
@@ -16,14 +16,35 @@ public class Cuentas {
 	public static void imprimirVentas(int[] ventas) {
 		System.out.println("Ventas diarias: " + Arrays.toString(ventas));
 		}
+	public static void topDeVentas(int[] ventas) {
+		System.out.println("Top de ventas: " + Arrays.toString(ventas));
+		}
+	public static int TotalVentasPorSemana(int[] ventas) {
+	    int total = 0;
+	    for (int i = 0; i < ventas.length; i++) {
+	        int venta = ventas[i];
+	        total += venta;
+	        }
+	    return total;
+	    }
+	public static int[] ventasMenorAMayor(int[] ventas) {
+        int[] ventasOrdenadas = Arrays.copyOf(ventas, ventas.length);
+        Arrays.sort(ventasOrdenadas);
+        return ventasOrdenadas;
+        }
 	public static void main(String[] args) {
 		int[][] ventas = ventasAleatorias();
 		for (int semana = 0; semana < 4; semana++) {
 			int[] ventasSemana = ventas[semana];
 			System.out.println("Semana " + (semana + 1) + ":");
 			imprimirVentas(ventasSemana);
+			int totalVentas = TotalVentasPorSemana(ventasSemana);
+            System.out.println("Total de ventas: " + totalVentas);
+            int[] top_ventas = ventasMenorAMayor(ventasSemana);
+            topDeVentas(top_ventas);
 			}
 		}
-	}
+    }
+    
 
 
